06608eef492e4a83884e2e7e558be741ee8fea27
shin <huutrimega@gmail.com>
2020-10-28 16:21:30 +0700

    .

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const path = require("path");
 const middlewares = jsonServer.defaults();
+
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 
 const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 
 server.use(router);
+
 server.listen(port, () => {
 	console.log("JSON Server is running");
 });
