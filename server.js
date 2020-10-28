f9f3d4ac292df993b60a68c3ce7949bc7bf2a6e8
shin <huutrimega@gmail.com>
2020-10-28 16:25:37 +0700

    update server

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const path = require("path");
 const middlewares = jsonServer.defaults();
 
 const router = jsonServer.router(path.join(__dirname, "db.json"));
-
 const port = process.env.PORT || 3001;
 
 server.use(middlewares);
-
 server.use(router);
 
 server.listen(port, () => {
